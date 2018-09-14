6c7f99ade66c919852fa9406a3751eab2f801224
whalen43 <36545787+whalen43@users.noreply.github.com>
2018-09-14 09:55:40 -0400

    Rajouter Login

---
+++ PROJET_FINAL/Amidelaposte/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Amidelaposte.Startup))]
+namespace Amidelaposte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
