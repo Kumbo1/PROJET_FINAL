4bc34095f7e4d3eb4bf5f2d021005af0ea116f6a
whalen43 <36545787+whalen43@users.noreply.github.com>
2018-09-21 22:51:03 -0400

    Ajout de RegisterController
    Avec un bug de conversion

--- PROJET_FINAL/PROJET_FINAL/Controllers/RegisterController.cs
+++ PROJET_FINAL/PROJET_FINAL/Controllers/RegisterController.cs
@@ -14,5 +14,40 @@ namespace PROJET_FINAL.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult RegisterClient(PROJET_FINAL.Client client)
+        {
+            var reg = new Register();
+            if (ModelState.IsValid)
+            {
+                using (ProjetDBEntities2 db = new ProjetDBEntities2())
+                {
+                    var queryUser = db.Clients.FirstOrDefault(u => u.Username == client.Username);
+                    if (queryUser == null)
+                    {
+                        reg.Prenom = client.PrenomClient;
+                        reg.Nom = client.NomClient;
+                        reg.Username = client.Username;
+                        reg.Adresse = client.Adresse;
+                        reg.CodePostal = client.CodePostal;
+                        reg.Telephone = client.Telephone;
+                        reg.EstMajeur = client.EstMajeur;
+                        reg.MotDePasse = client.MotDePasse;
+                        reg.Courriel = client.Courriel;
+                        db.Clients.Add(reg);
+                        db.SaveChanges();
+                        return RedirectToAction("Login");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Register");
+                    }
+                                  
+                }
+            }
+            
+            return View();
+        }
     }
 }
