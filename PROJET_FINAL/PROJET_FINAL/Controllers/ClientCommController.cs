4e3e06d656e42d881f94d921b5e34095a5fde4b1
whalen43 <36545787+whalen43@users.noreply.github.com>
2018-09-26 14:37:11 -0400

    Ajout Truc de commande
    ajout de truc pour commande

---
+++ PROJET_FINAL/PROJET_FINAL/Controllers/ClientCommController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace PROJET_FINAL.Controllers
+{
+    public class ClientCommController : Controller
+    {
+        // GET: ClientComm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AjouterCommande(Commande comm)
+        {
+
+            if (ModelState.IsValid)
+            {
+                using (ProjetDBEntities2 db = new ProjetDBEntities2())
+                {                  
+                        var date = DateTime.Now.ToString("M/dd/yyyy");
+                        var infosup = new SqlParameter("pinfosup", comm.InfosSup);
+                        var estmajeur = new SqlParameter("pestmajeur", comm.EstMajeur);                        
+                        var adresse = new SqlParameter("padresse", comm.Adresse);
+                        var codepostal = new SqlParameter("pcodepostal", comm.CodePostal);                       
+                        db.Database.ExecuteSqlCommand("execute AjouterCommande @pinfosup, @pdate, @pestmajeur" +
+                            ", @padresse, @pcodepostal", infosup, date, estmajeur, adresse, codepostal);
+                        return RedirectToAction("Index", "Welcome");                   
+                }
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "Remplissez tous les espaces correctement");
+                return RedirectToAction("Index", "Register");
+            }
+        }
+    }
+}
---
+++ PROJET_FINAL/PROJET_FINAL/Models/ClientModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJET_FINAL.Models
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class CommandeClient
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CommandeClient()
+        {
+            this.Objets = new HashSet<Objet>();
+        }
+
+        public int IdCommande { get; set; }
+        public int IdClient { get; set; }
+        public Nullable<int> idLivreur { get; set; }
+        public string InfosSup { get; set; }
+        public System.DateTime Date { get; set; }
+        public bool EstMajeur { get; set; }
+        public string Adresse { get; set; }
+        public Nullable<int> IdVille { get; set; }
+        public string CodePostal { get; set; }
+
+        public virtual Client Client { get; set; }
+        public virtual Livreur Livreur { get; set; }
+        public virtual Ville Ville { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Objet> Objets { get; set; }
+    }
+}
