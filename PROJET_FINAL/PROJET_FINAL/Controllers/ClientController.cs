b9ca54a523143a1aa93ed675166d7f04fc8c0754
whalen43 <36545787+whalen43@users.noreply.github.com>
2018-09-26 10:32:30 -0400

    Client controller and view (incomplet)
    a refaire mais bonne base

---
+++ PROJET_FINAL/PROJET_FINAL/Controllers/ClientController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJET_FINAL.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AjouterCommande()
+        {
+            using (var db = new ProjetDBEntities2())
+            {
+                string info = Request.Form["infosup"];
+                var date = DateTime.Now;
+                string majeur = Request.Form["majeur"];
+                string adresse = Request.Form["adresse"];
+                string codepostal = Request.Form["codepostale"];
+
+            }
+                return View();
+        }
+    }
+}
