a695e9ed6351667833df146b8d3b86874ae0327e
whalen43 <36545787+whalen43@users.noreply.github.com>
2018-10-08 15:38:38 -0400

    Trucs pour afficher les commandes
    qui marche pas encore

---
+++ PROJET_FINAL/PROJET_FINAL/Controllers/CommCourantController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using PROJET_FINAL.Models;
+
+namespace PROJET_FINAL.Controllers
+{
+    public class CommCourantController : Controller
+    {
+        // GET: CommCourant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VoirCommande(LesCommandes commande)
+        {           
+            using (ProjetDBEntities2 db = new ProjetDBEntities2())
+            {              
+                return View(db.Commandes.ToList());
+            }
+
+                
+        }
+    }
+}
---
+++ PROJET_FINAL/PROJET_FINAL/Models/LesCommandes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJET_FINAL.Models
+{
+    public class LesCommandes
+    {
+        public int IdCommande { get; set; }
+        public string InfoSup { get; set; }
+        public DateTime DateCommande { get; set; }
+        public string NomObjet { get; set; }
+        public float Prix { get; set; }
+    }
+}
